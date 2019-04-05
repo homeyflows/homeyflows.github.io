18dd86e845bf28dfa71f842f6a240b2600f5937f
homeyflows <49301374+homeyflows@users.noreply.github.com>
2019-04-05 12:31:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ var linkMenu = [
   ["JSFiddle",                 "",""],
   ["Stack Overflow",           "",""],
   
-  [svgDownload,                 "magenta",                                     "-HEAD-"], // Download 18
+  [svgDownload 18,                 "magenta",                                     "-HEAD-"], // Download 18
   ["Steam",                    "",""],
   ["Humble Bundle",            "",""],
   ["GOG.com",                  "",""],
